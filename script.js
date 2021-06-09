1072d1bf6628ff7f4fe21dc46c720a3c0d4cd2eb
fiton <fb45810@ubt-uni.net>
2021-06-09 02:02:24 +0200

    minor fixes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const form = document.getElementById('form')
 const errorElement = document.getElementById('error')
 
 
-form.addEventListener('submit', (e) => ){
+form.addEventListener('submit', (e) => {
     if(password.value.length <=6){
         messages.push('Fjalekalimi duhet te jete me i gjate se 6 karaktere!')
     }
@@ -12,12 +12,12 @@ form.addEventListener('submit', (e) => ){
     if(password.value.length >=20){
         messages.push('Fjalekalimi duhet te jete me i shkurter se 20 karaktere!')
     }
-}
 
-if(messages.length > 0){
+
+    if(messages.length > 0){
     e.preventDefault()
     errorElement.innerText = messages.join(', ')
-}
+    }
 
 
 })
